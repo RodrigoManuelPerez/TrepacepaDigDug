8ddd196f43ee2519d410ce090cd017a6a62c58bb
RodrigoManuelPerez <32597366+RodrigoManuelPerez@users.noreply.github.com>
2017-11-28 22:09:08 +0100

    Main actualizado para la url

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ var PlayScene = require('./play_scene.js');
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
+    this.game.load.baseURL = 'https://rodrigomanuelperez.github.io/TrepacepaDigDug/src/';
+    this.game.load.crossOrigin = 'anonymous';
     this.game.load.image('preloader_bar', 'images/preloader_bar.png');
   },
 
