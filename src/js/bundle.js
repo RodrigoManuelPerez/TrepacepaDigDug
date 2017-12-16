d8d1ae2e58e1627128c719e66e17db7f371b7e8f
Unknown <rodrigo.manuel.perez@gmail.com>
2017-12-16 16:24:39 +0100

    Ahora

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -912,19 +912,22 @@ var PlayScene = {
                             this.game.world.addChild(BloqTierra);
                             tierra.add(BloqTierra);
 
-                            var PosRock = new Par(i, j-1);
+                            var PosRock = new Par(posX-40, posY-44);
                             var Rock = new Roca(this.game, PosColl, 'RocaCompleta', 'Roca', 'RocaCompletaSpriteSheet');
                             this.game.physics.arcade.enable(Coll); 
-                            
                             roca.add(Coll);     //AÑADIMOS AL GRUPO
-
                             
                         }
                         else if(row[i]=='5'){    //Enemigo
                             
-                            
+                            var PosEne = new Par(i+20,j+20);
+                            var enemigo = new Enemy(this.game,PosEne,'Slime','Enemigo',limiteDerecho, limiteSuperior,player);
+                            this.game.physics.enable(enemigo, Phaser.Physics.ARCADE);
+                            enemigo.anchor.x = 0.5;
+                            enemigo.anchor.y = 0.5;
+                            this.game.world.addChild(enemigo);
+                            GrupoEnemigos.add(enemigo);
 
-                            posX+=43;
                         }
                         else{
                             posX+=43;
@@ -970,7 +973,7 @@ var PlayScene = {
                     this.game.physics.enable(enemigo, Phaser.Physics.ARCADE);
                     enemigo.anchor.x = 0.5;
                     enemigo.anchor.y = 0.5;
-                    //this.game.world.addChild(enemigo);
+                    this.game.world.addChild(enemigo);
                     GrupoEnemigos.add(enemigo);
                 
                 }
